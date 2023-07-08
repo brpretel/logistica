48fa6515ae3657b3bfa3fa2d25704909e5f4041d
brpretel <samipretel@hotmail.com>
2023-07-08 15:22:20 -0500

    Cambios

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ import databases
 import sqlalchemy
 from decouple import config
 
-DATABASE_URL = f"postgresql://postgres:v5ZtSSMP4DogMNQW8YdO@containers-us-west-177.railway.app:6326/railway"
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/logistica"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
